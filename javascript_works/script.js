9f8e3bf90689175d441efa885caaeb83d73710fe
Aysha Sherin <ayshasherin6199@gmail.com>
2021-09-09 11:53:57 +0530

    msgs

---
+++ javascript_works/script.js
@@ -0,0 +1,23 @@
+// javascipt programming
+
+// to print a message  in console
+
+
+// console.log() => javascript
+
+// --------------------identifiers----------------------- 
+// identification purpose nn use cheyyunnathaan identifiers
+
+// console.log("hello world");
+// var company_name="luminar technolab";
+// var loc_name="kakkanad";
+// console.log(company_name);
+// console.log(loc_name);
+
+var name="Aysha Sherin";
+const age="21";
+console.log(name);
+console.log(age);
+console.log("Hai i am Aysha Sherin");
+
+
