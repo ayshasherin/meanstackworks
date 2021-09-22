58332885fe9aec453c7dd05b46b7b35f0b127d5f
Aysha Sherin <ayshasherin6199@gmail.com>
2021-09-22 11:47:31 +0530

    hey

--- javascript_works/array_pgms/search15.js
+++ javascript_works/array_pgms/search15.js
@@ -1,18 +1,19 @@
-// var arr=[10,11,12,13,14,11];
-// for(let num of arr){
-//     if(num==15){
-//         console.log("Element is found");
-//     }
-//     else{
-//         console.log("Element is not found");
-//     }
-//     break
-// }
-
-
 var arr=[10,11,12,13,14,11];
+var element=12;
+var flag=0;
 for(let num of arr){
-    var res=num==15?"Element is found":"Element is not found"
+    if(num==element){
+        flag+=1;
+    }  
+    
 }
-console.log(res);
+
+if(flag==0){
+    console.log("element not found");
+}
+else{
+    console.log("element found");
+}
+
+
 
